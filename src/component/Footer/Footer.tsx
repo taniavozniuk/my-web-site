23a6068cface2b106e0e87da6b4ce9c5218ae843
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-11-03 17:58:45 +0200

    modile

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,12 +1,20 @@
 import { NavLink } from "react-router-dom";
-import { link } from "./link";
+import { link, linkMoblide } from "./link";
 import styles from "./Footer.module.scss";
 import { useTranslation } from "react-i18next";
 
 export const Footer = () => {
   const { t } = useTranslation();
   return (
-    <div className={styles.footer}>
+    <>
+      <div className={styles.footer}>
+        <div className={styles.wrapQuestion}>
+          <h2 className={styles.question}>{t("questions")}</h2>
+          <a className={styles.button} href="mailto:taniavoznyk19655@gmail.com">
+            {t("btques")}
+          </a>
+        </div>
+      </div>
       <ul className={styles.ul}>
         {link.map((l) => (
           <li key={l.id}>
@@ -16,12 +24,16 @@ export const Footer = () => {
           </li>
         ))}
       </ul>
-      <div className={styles.wrapQuestion}>
-        <h2 className={styles.question}>{t("questions")}</h2>
-        <a className={styles.button} href="mailto:taniavoznyk19655@gmail.com">
-          {t("btques")}
-        </a>
-      </div>
-    </div>
+
+      <ul className={styles.ulM}>
+        {linkMoblide.map((l) => (
+          <li key={l.id}>
+            <NavLink to={l.link} className={styles.li} target="_blank">
+              <img src={l.image} alt={l.name} className={styles.icon} />
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
--- src/component/Footer/link.tsx
+++ src/component/Footer/link.tsx
@@ -8,3 +8,30 @@ export const link = [
   },
   { id: 4, name: "Telegram", link: "t.me/Vozniuk_1" },
 ];
+
+export const linkMoblide = [
+  {
+    id: 1,
+    name: "Email",
+    link: "mailto:taniavoznyk19655@gmail.com",
+    image: "image/icon/email.svg",
+  },
+  {
+    id: 2,
+    name: "Github",
+    link: "https://github.com/taniavozniuk",
+    image: "image/icon/git.svg",
+  },
+  {
+    id: 3,
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/tania-vozniuk-3192432a3/",
+    image: "image/icon/linkedin.svg",
+  },
+  {
+    id: 4,
+    name: "Telegram",
+    link: "t.me/Vozniuk_1",
+    image: "image/icon/telegram.svg",
+  },
+];
